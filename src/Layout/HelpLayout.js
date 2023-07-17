d53c3a1dee337feb7b1ef4cf9c5bb6ec1d4604fb
belgotech <belovedobiora2002@gmail.com>
2023-07-17 23:28:33 +0100

    New Commit

--- src/Layout/HelpLayout.js
+++ src/Layout/HelpLayout.js
@@ -3,8 +3,7 @@ import { NavLink, Outlet } from "react-router-dom"
 const HelpLayout =()=>{
     return(
         <div className="help-layout">
-            HelpLayout
-
+            
             <h2>Website Help</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, asperiores.</p>
 
