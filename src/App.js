240c64fd12bd0bfe5159d95a3926031af6318fc0
belgotech <belovedobiora2002@gmail.com>
2023-07-17 21:33:06 +0100

    Modernizing basic route

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom"
+import { 
+  createBrowserRouter,
+  Routes, 
+  Route, 
+  createRoutesFromElements
+} from "react-router-dom"
 
 //Pages
 import Home from "./Pages/Home"
 import About from "./Pages/About"
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <header>
-        <nav>
-          <h1>Jobby</h1>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="about">About</NavLink>
-        </nav>
-      </header>
-      <main>
+const router = createBrowserRouter(
+  createRoutesFromElements(
         <Routes>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
         </Routes>
+  )
+)
+
+const App = () => {
+  return (
+  
+      // <header>
+      //   <nav>
+      //     <h1>Jobby</h1>
+      //     <NavLink to="/">Home</NavLink>
+      //     <NavLink to="about">About</NavLink>
+      //   </nav>
+      // </header>
+      <main>
       </main>
-    </BrowserRouter>
+    
   )
 }
 
