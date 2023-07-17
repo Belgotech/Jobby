675a4060c154b4efeb38d81ed7ac3e31cee3d288
belgotech <belovedobiora2002@gmail.com>
2023-07-17 12:34:51 +0100

    Adding Basic Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 
+//Pages
+import Home from "./Pages/Home"
+import About from "./Pages/About"
+
 const App = () => {
   return (
     <BrowserRouter>
+    <header>
+      <nav>
+        <h1>Welcome to Jobby</h1>
+        <Link to="/">Home</Link>
+        <Link to="about">About</Link>
+      </nav>
+    </header>
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
