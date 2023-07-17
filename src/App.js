568e36c6dd53d549b9159d1d1fae2cfd3924cbc5
belgotech <belovedobiora2002@gmail.com>
2023-07-17 12:50:25 +0100

    New Commit

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ import About from "./Pages/About"
 const App = () => {
   return (
     <BrowserRouter>
-    <header>
-      <nav>
-        <h1>Jobby</h1>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="about">About</NavLink>
-      </nav>
-    </header>
+      <header>
+        <nav>
+          <h1>Jobby</h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="about">About</NavLink>
+        </nav>
+      </header>
       <main>
         <Routes>
-          <Route index  element={<Home />} />
+          <Route index element={<Home />} />
           <Route path="about" element={<About />} />
         </Routes>
       </main>
