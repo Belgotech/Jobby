002462ec1f7636dbdc7187c6064aedb33eafbc0b
belgotech <belovedobiora2002@gmail.com>
2023-07-18 12:01:40 +0100

    App Optimization

--- src/Pages/Help/Faq.js
+++ src/Pages/Help/Faq.js
@@ -1,12 +1,53 @@
+const data = [
+    {
+        id: 1,
+        title: "Lorem ipsum dolor sit amet.",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim reiciendis reprehenderit quis nesciunt obcaecati, minima blanditiis nisi consequatur nihil non debitis dolorem delectus excepturi perferendis amet ab aliquam, iusto neque?"
+    },
+    {
+        id: 2,
+        title: "Lorem ipsum dolor sit amet.",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim reiciendis reprehenderit quis nesciunt obcaecati, minima blanditiis nisi consequatur nihil non debitis dolorem delectus excepturi perferendis amet ab aliquam, iusto neque?"
+    },
+    {
+        id: 3,
+        title: "Lorem ipsum dolor sit amet.",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim reiciendis reprehenderit quis nesciunt obcaecati, minima blanditiis nisi consequatur nihil non debitis dolorem delectus excepturi perferendis amet ab aliquam, iusto neque?"
+    },
+    {
+        id: 4,
+        title: "Lorem ipsum dolor sit amet.",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim reiciendis reprehenderit quis nesciunt obcaecati, minima blanditiis nisi consequatur nihil non debitis dolorem delectus excepturi perferendis amet ab aliquam, iusto neque?"
+    },
+    {
+        id: 5,
+        title: "Lorem ipsum dolor sit amet.",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim reiciendis reprehenderit quis nesciunt obcaecati, minima blanditiis nisi consequatur nihil non debitis dolorem delectus excepturi perferendis amet ab aliquam, iusto neque?"
+    },
+    {
+        id: 6,
+        title: "Lorem ipsum dolor sit amet.",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim reiciendis reprehenderit quis nesciunt obcaecati, minima blanditiis nisi consequatur nihil non debitis dolorem delectus excepturi perferendis amet ab aliquam, iusto neque?"
+    },
+]
+
+
 const Faq = () => {
     return (
         <div className="faq">
             <h3>Frequently Asked Questions</h3>
 
-            <div className="question">
-                <p><strong>Lorem ipsum dolor sit amet.</strong></p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, recusandae doloribus qui molestias similique asperiores incidunt. Obcaecati tenetur consectetur dolorem eius ex, ad, laudantium inventore quia odio minus eligendi ipsa?</p>
-            </div>
+            
+                {data.map((id, title, description)=>{
+                    return(
+                        <div key={id} className="question" >
+                            <p><strong>{title}</strong></p>
+                            <p>{description}</p>
+                        </div>
+                        
+                    )
+                })}
+           
 
             <div className="question">
                 <p><strong>Lorem ipsum dolor sit amet.</strong></p>
