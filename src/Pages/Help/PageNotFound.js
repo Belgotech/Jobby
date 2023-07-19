f2535d712c35515f807c4ff53b127d589294823e
belgotech <belovedobiora2002@gmail.com>
2023-07-19 20:15:38 +0100

    Erro Handling

--- src/Pages/Help/PageNotFound.js
+++ src/Pages/Help/PageNotFound.js
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom"
+
 const PageNotFound = ()=>{
     return(
         <div className="pageNotFound">
             <h2>Page Not Found</h2>
-            <h2>Page Not Found</h2>
-            <h2>Page Not Found</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro laborum repudiandae tempore repellendus dolorem harum autem illum voluptate unde. Ullam quia suscipit tenetur reprehenderit nemo commodi rerum sunt, voluptas dicta.</p>
+
+            <p>Go to<Link to="/">Homepage</Link>.</p>
         </div>
     )
 }
