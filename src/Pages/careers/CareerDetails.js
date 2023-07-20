b42fa9c1966d10e6f41124f0c93c7634e18c3077
belgotech <belovedobiora2002@gmail.com>
2023-07-20 22:36:15 +0100

    New Commit

--- src/Pages/careers/CareerDetails.js
+++ src/Pages/careers/CareerDetails.js
@@ -1,11 +1,23 @@
-import { useParams } from "react-router-dom"
+import { useLoaderData, useParams } from "react-router-dom"
 
 export default function CareersDetails() {
-  const { id } = useParams
+  const { id } = useParams()
+  const career = useLoaderData()
 
   return (
 <div className="careers-details">
-     <h2>{ id }</h2>
+     <h2>Career Details for{career.title}</h2>
+     <p>Starting Salary :{career.salary}</p>
+     <p>Location:{career.location} </p>
     </div>
   )
+}
+
+// loader function
+export const CareerDetailsLoader = async ({ params })=>{
+  const { id } = params
+
+  const res = await fetch('https://localhost:4000/careers', + id )
+
+  return res.json()
 }
