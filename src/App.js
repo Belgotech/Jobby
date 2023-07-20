e9fc0476143978132c2853264c3471f892da1973
belgotech <belovedobiora2002@gmail.com>
2023-07-20 10:21:39 +0100

    Adding Loaders

--- src/App.js
+++ src/App.js
@@ -11,22 +11,29 @@ import About from "./Pages/About"
 import Faq from "./Pages/Help/Faq"
 import Contact from "./Pages/Help/Contact"
 import PageNotFound from "./Pages/Help/PageNotFound"
+import Careers from "./Pages/careers/Careers"
 
 //Layout
 import RootLayout from "./Layout/RootLayout"
 import HelpLayout from "./Layout/HelpLayout"
+import CareersLayout from "./Layout/CareersLayout"
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />} >
       <Route index element={<Home />} />
       <Route path="about" element={<About />} />
+
       <Route path="help" element={<HelpLayout />} >
         <Route path="faq" element={<Faq />} />
         <Route path="contact" element={<Contact />} />
       </Route>
 
-      <Route path="*" element={<PageNotFound />}/>
+      <Route path="careers" element={<CareersLayout />}>
+        <Route index element={<Careers />} />
+      </Route>
+
+      <Route path="*" element={<PageNotFound />} />
     </Route>
   )
 )
